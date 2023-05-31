d52357f2920777036a19f573feb2c280df6f62fd
Nana Kwame Osei Ofori Dickson <dic20016@byui.edu>
2023-05-31 16:57:27 +0000

    Assignment 1

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req,res) => {
-  res.send("Hello, Nana Kwame");
+  res.send("Hello, Nana Kwame Dickson");
 });
 
 const port = 3000;
