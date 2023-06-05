79e3a1d655a860056233219c4bbe3274971189de
Nana Kwame Osei Ofori Dickson <dic20016@byui.edu>
2023-06-05 16:49:34 +0000

    Fixed the listner

---
+++ routes/index.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const routes = express.Router();
+const lesson1Controller = require('../controllers/lesson1.js');
+
+
+routes.get('/', lesson1Controller.jasmineRoute);
+routes.get('/emily', lesson1Controller.emilyRoute);
+routes.get('/hannah', lesson1Controller.hannahRoute);
+
+
+module.exports =routes;
