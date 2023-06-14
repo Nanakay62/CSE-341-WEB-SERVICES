0b98ad462444ba131867d23541aa430f46daaab5
Nana Kwame Osei Ofori Dickson <dic20016@byui.edu>
2023-06-14 21:59:36 +0000

    Updated server.js

--- server.js
+++ server.js
@@ -11,6 +11,11 @@ const port = 3000;
 
 app.use(express.json());
 app.use(bodyParser.json());
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  next();
+});
+app.use('/', routes);
 
 // MongoDB connection
 const MONGODB_URI = process.env.MONGODB_URI;
@@ -20,12 +25,11 @@ initDb((err) => {
   if (err) {
     console.error('Sorry, there was an error connecting to the database', err);
     process.exit(1);
+  } else {
+    app.listen(process.env.PORT || port, () => {
+      console.log('Web Server is listening at port ' + (process.env.PORT || port));
+    console.log('Database connected');
+  });
   }
-  console.log('Database connected');
 });
 
-app.use('/', routes);
-
-app.listen(MONGODB_URI || port, () => {
-  console.log('Web Server is listening at port ' + (MONGODB_URI || port));
-});
